bf3db180809ee118c641439e001baa665c2f87c0
KseniaKoroleva <kseniakoroleva13@gmail.com>
2015-04-25 22:18:56 -0700

    Update cachematrix.R
    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -17,8 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-##The second function computes the inverse of the special "matrix"
-##returned by makeCacheMatrix above.
+##The second function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ##If the inverse has already been calculated (and the matrix has not changed),
 ##then this function retrieves the inverse from the cache.
 
